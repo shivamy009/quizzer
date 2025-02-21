554711cc47e25d81e28298bb9ce697f2b5e7fe2d
shivamy009 <shivam0342005@gmail.com>
2025-02-21 09:25:38 +0530

    first commit

---
+++ client/src/components/PageTitle.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function PageTitle({title}) {
+  return (
+    <div className='mt-2' style={{ cursor: 'pointer' }}>
+  <h1>{title}</h1>
+</div>
+  )
+}
+
+export default PageTitle
